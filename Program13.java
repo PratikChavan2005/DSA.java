f147fa19732a22f0156110c05d8f6d9d71eea93b
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-08-04 19:56:59 +0530

    Add files via upload

---
+++ Program13.java
@@ -0,0 +1,14 @@
+class Program13{
+   public static void main(String[]args){
+   int n=5;
+int k=0;
+ for(int i=1;i<=5;i++){
+     for(int j=1;j<=i;j++){
+   
+    k=k+1;
+System.out.print(k);
+  }  
+System.out.println();
+ }
+}
+}
