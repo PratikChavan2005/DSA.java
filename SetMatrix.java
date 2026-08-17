a9a06364006f1a50991bc89462df94f83d9fa383
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-08-17 21:05:54 +0530

    Add files via upload

---
+++ SetMatrix.java
@@ -0,0 +1,44 @@
+class SetZeros{
+	
+	public static void main(String[] args){
+	
+	
+		int[][] matrix={{1,1,1},{1,0,1},{1,1,1}};
+		int n=matrix.length;
+		int m=matrix[0].length;
+		boolean[] row=new boolean[n];
+		boolean[] col= new boolean[m];
+
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if(matrix[i][j]==0){
+					row[i]=true;
+					col[j]=true;
+				}
+			}
+		}
+		
+		for(int i=0;i<n;i++){
+			if(row[i]){
+				for(int j=0;j<m;j++){
+					matrix[i][j]=0;	
+				}
+			}	
+		}
+		
+		for(int j=0;j<m;j++){
+			if(col[j]){
+				for(int i=0;i<n;j++){
+					matrix[i][j]=0;
+				}	
+			}
+		}
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				System.out.print(matrix[i][j]);
+			}
+			System.out.println();
+		}
+	}	
+	
+}
