1fe4429895b94c585066156b1d536d7422e892de
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-08-27 21:02:45 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,22 @@
+class calculator{
+  public static void main(String [] args){
+    //calculator c = new calculator();
+    int result = addition (5,52);
+    System.out.println(result);
+
+    result= div(63,1);
+   System.out.println(result);   
+
+  }
+      public static int addition(int a ,int b){
+      return a+b;
+	
+      }
+
+      public static int div(int a , int b){
+	
+      return a/b;
+	
+     }
+
+}
