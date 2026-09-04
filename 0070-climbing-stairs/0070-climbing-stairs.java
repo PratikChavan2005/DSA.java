ac202ef0da40355d1b8a3cb1c2c17fbc4a25b93c
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-09-04 22:28:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.8%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        int first=1;
+        int second = 2;
+        if(n<=2){
+            return n;
+        }
+        for(int i=3;i<=n;i++){
+            int current = first+second;
+            first=second;
+            second=current;
+        }
+        return second;
+    }
+}
